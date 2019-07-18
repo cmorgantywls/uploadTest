838c01da6648df6382b1d0e3d68dcfb77f60a21b
cmorgantywls <cmorgan@tywlsbronx.org>
2019-07-18 15:26:49 +0000

    I wanna see version control. >:(

--- testing.py
+++ testing.py
@@ -1,5 +1,14 @@
 def shout(thing):
     return thing.upper()
+    
+    #rm -rf .git == remove/break all links to original you cloned down
+    #git remote show origin == is it linked to anything on github??
+    #git init == initialize as a git repo
+    #git status == check what needs to be updated
+    # git add == add files to sync/upload to github
+    # git commit -m "your message" == commit changes to the master branch on github
+    #git push == push everything through to github
+    #making even more messages about THINGS
 
 def isLeapYear(year):
     if year%4==0:
